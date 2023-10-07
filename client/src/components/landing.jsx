7d10446daeb8a6c2de102c213a39d5ba970efb22
Ivan Zheng <zivan7644@gmail.com>
2023-10-07 12:30:56 -0400

    front end init

---
+++ client/src/components/landing.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../css/landing.css";
+
+const Landing = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="text-wrapper">Your Name</div>
+          <div className="text-wrapper-2">(forum name)</div>
+        </div>
+        <div className="text-wrapper-3">HackRU Goated Project</div>
+        <img
+          className="arrow"
+          alt="Arrow"
+          src="https://cdn.animaapp.com/projects/65217f2625a5f136eb81c527/releases/65217fdf819c5f5f1d286d06/img/arrow-1-1@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Landing
