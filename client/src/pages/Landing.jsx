4d102221726e74356c0bf8164c35b9bfb0ab7d79
Terrence Zhang <96602253+ZhangTerrence@users.noreply.github.com>
2023-10-07 17:42:22 -0400

    d

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,7 +1,6 @@
-import { AuthButton } from "../components/AuthButton";
-import "../css/landing.css";
 import { Navbar } from "../components/Navbar.jsx";
-import {motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import "../css/landing.css";
 
 export const Landing = () => {
   // var fontString = document.querySelector("div.project-name").style.fontSize;
@@ -25,16 +24,19 @@ export const Landing = () => {
   var { scrollYProgress } = useScroll();
   const scale = useTransform(scrollYProgress, [0, 0.4], [1, 0.25]);
   const pos = useTransform(scrollYProgress, [0, 1], [0, -500]);
-  document.querySelector("img.arrow").addEventListener("click", () => {
+  document.querySelector(".arrow").addEventListener("click", () => {
     window.scrollTo({
       top: 315,
-      behavior: "smooth"});
+      behavior: "smooth",
+    });
   });
+
   return (
-    
     <div className="index">
-        <Navbar/>
-        <motion.div layoutScroll className="project-name" 
+      <Navbar />
+      <motion.div
+        layoutScroll
+        className="project-name"
         // animate={{
         //   rotate: 180
         // }}
@@ -44,27 +46,32 @@ export const Landing = () => {
         //   duration: 2,
         //   type: "keyframes"
         // }}
-        style={{scale, translateY: pos}}>
-          <motion.div layout className="project-name-child" style={ {fontSize: scrollYProgress}}/>
-          HackRU Goated Project
-          </motion.div>
-        <motion.img
-          animate={{
-            translateY: [0, 10, 0]
-          }}
-          whileHover={{
-            scale: 1.2
-          }}
-          transition={{
-            repeat: Infinity,
-            repeatType: "mirror",
-            duration: 0.3,
-            repeatDelay: 1
-          }}
-          className="arrow"
-          alt="Arrow"
-          src="https://cdn.animaapp.com/projects/65217f2625a5f136eb81c527/releases/65217fdf819c5f5f1d286d06/img/arrow-1-1@2x.png"
+        style={{ scale, translateY: pos }}
+      >
+        <motion.div
+          layout
+          className="project-name-child"
+          style={{ fontSize: scrollYProgress }}
         />
+        HackRU Goated Project
+      </motion.div>
+      <motion.img
+        animate={{
+          translateY: [0, 10, 0],
+        }}
+        whileHover={{
+          scale: 1.2,
+        }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 0.3,
+          repeatDelay: 1,
+        }}
+        className="arrow"
+        alt="Arrow"
+        src="https://cdn.animaapp.com/projects/65217f2625a5f136eb81c527/releases/65217fdf819c5f5f1d286d06/img/arrow-1-1@2x.png"
+      />
     </div>
   );
 };
